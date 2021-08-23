3fd4decf8743103be7c5d46e7cd72916ab87bf06
MasterMedo <mislav.vuletic@gmail.com>
2021-08-23 23:11:55 +0200

    simplify scraper

--- datasets/muscles/exrx.net/scraper.py
+++ datasets/muscles/exrx.net/scraper.py
@@ -1,12 +1,8 @@
-import os
 import csv
-import requests
 
 from bs4 import BeautifulSoup
 
 
-muscles_filename = "muscles.csv"
-bodyparts_filename = "bodyparts.csv"
 fieldnames_m = [
     "id",
     "name",
@@ -21,6 +17,7 @@ fieldnames_bp = [
 with open("data.txt") as f:
     html = f.read()
 
+
 def scrape_muscles(writer_bp: csv.DictWriter):
     soup = BeautifulSoup(html, "html.parser")
     columns = soup.find_all("div", class_="col-sm-6")
@@ -28,15 +25,15 @@ def scrape_muscles(writer_bp: csv.DictWriter):
     for col in columns:
         col = col.find("ul")
         for li in col.find_all("li", recursive=False):
-            bp = li.find("a").text.strip()
+            body_part = li.find("a").text.strip()
             fields = {
-                "id": bp.lower().replace(" ", "-"),
-                "name": bp,
+                "id": body_part.lower().replace(" ", "-"),
+                "name": body_part,
             }
             writer_bp.writerow(fields)
 
 
-with open(bodyparts_filename, "w") as f:
+with open("bodyparts.csv", "w") as f:
     writer_bp = csv.DictWriter(f, fieldnames=fieldnames_bp, lineterminator="\n")
     writer_bp.writeheader()
     scrape_muscles(writer_bp)
